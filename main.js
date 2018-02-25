95054cf4608f0dcb889b2a3d92c23b5fdd6712cb
NiclasV <niclas.victorsson@medieinstitutet.se>
2018-02-25 14:14:53 +0100

    second commit

--- main.js
+++ main.js
@@ -1,31 +1,39 @@
-//Todos Object
-var todos = [
-    {
-      todo: 'todo 1',
-      isDone: false
-    },
-    {
-      todo: 'todo 2',
-      isDone: false
-    },
-    {
-      todo: 'todo 3',
-      isDone: false
-    }
-];
+//Declaring a todo as an object
+var todo = Object();
+var todo = {
+  task: "Diska", 
+  isDone: false
+};
 
-//Saving all todos in localstorage
-localStorage.setItem("todos", JSON.stringify(todos));
+//Creating an array to collect the todo-objects
+var todos = [];
+
+//Pushing the pre-created todo to the array and console-logs the result (to see that it works)
+todos.push(todo);
+console.log(todos);
 
 //Add a constant for the todo add-button.
 const addBtn = document.getElementById("add-btn");
 
-//Create an click event for it
-addBtn.addEventListener('click', function() {
-    console.log('it\'s clickin bitch');
+//Create an click-event for it
+addBtn.addEventListener('click', function(event) {
+  //Preventing defaultevent with refreshing site at click
+  event.preventDefault()
+  //Get the value from the inputfield to the variable newTodo
+  var newTodo = document.getElementById('todo-input').value
+
+  console.log("the new task is\: " + newTodo)
+
+  //Creating a new todo with the value from the input-field
+  var todo = {
+    task: newTodo,
+    isDone: false
+  };
+
+  //Pushing the new todo to the todos-array created before
+  todos.push(todo);
+  console.log(todos)
+
 });
 
-//loop out all todos from localstorage (based on the length)
-for (var i = 0; i < localStorage.length; i++){
-   console.log(localStorage.getItem(localStorage.key(i)));
-}
+  
